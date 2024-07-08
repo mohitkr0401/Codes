2ad97e8348a5a26b8fd79df60a587ee0406185d1
mohitkr0401 <mohitkr0401@gmail.com>
2024-07-09 03:11:56 +0530

    learned

--- JavaScript/js-hindi-youtube-main/05_iterations/07_map.js
+++ JavaScript/js-hindi-youtube-main/05_iterations/07_map.js
@@ -4,9 +4,16 @@ const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 // const newNums = myNumers.map( (num) => { return num + 10})
 
+
+//chaining....  multiple maps and filters ko ek sath use kar skte hai, sabko dot(.) se separate karke likhna hota hai
 const newNums = myNumers
                 .map((num) => num * 10 )
                 .map( (num) => num + 1)
                 .filter( (num) => num >= 40)
 
 console.log(newNums);
+
+
+
+// filter , true and false ke liye use hota.....condition ke hisab se return karta
+// map, ek ek element ko iterate karta hai, aur usko modify karta hai, and sab kuch return karta
--- JavaScript/js-hindi-youtube-main/05_iterations/08_reduce.js
+++ JavaScript/js-hindi-youtube-main/05_iterations/08_reduce.js
@@ -1,9 +1,24 @@
+// reduce continous adding ke liye use hota hain, like shopping cart bills and all
+// reduce is a higher order function
+// reduce is a method of array
+// initial value ko bhejna pdhta hai (,) ke baad.....jo ek starting point deta hai
+
+
+
 const myNums = [1, 2, 3]
+
+
+
+// explain karne ke liye, with explicit return
+
 
 // const myTotal = myNums.reduce(function (acc, currval) {
 //     console.log(`acc: ${acc} and currval: ${currval}`);
 //     return acc + currval
 // }, 0)
+
+
+// same chiz with arrow function , easy to understand
 
 const myTotal = myNums.reduce( (acc, curr) => acc+curr, 0)
 
@@ -28,6 +43,9 @@ const shoppingCart = [
         price: 12999
     },
 ]
+
+// abhi toh aise hi normally add kar diya, actual project ke time pr type checkimg bhi krni hogi, like string ya NAN na ho
+// item.price isliye, taaki array of objects se key ki value le sake.
 
 const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
 
