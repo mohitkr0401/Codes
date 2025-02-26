d557ee93a1111b106a13d521863768ea16024e05
mohitkr0401 <mohitkr0401@gmail.com>
2025-02-26 21:55:02 +0530

    reversing words in a string

---
+++ DSA/String/ReverseWord.cpp
@@ -0,0 +1,26 @@
+// Reverse the words in a string
+// Given an input string s, reverse the order of the words.
+
+//Time Complexity: O(n)
+
+class Solution {
+    public:
+        string reverseWords(string s) {
+            int n = s.length();
+            string ans = "";
+            reverse(s.begin(), s.end());
+    
+            for (int i = 0; i < n; i++) {
+                string word = "";
+                while (i < n && s[i] != ' ') {
+                    word += s[i];
+                    i++;
+                }
+                reverse(word.begin(), word.end());
+                if (word.length() > 0) {
+                    ans += " " + word;
+                }
+            }
+            return ans.substr(1);
+        }
+    };
