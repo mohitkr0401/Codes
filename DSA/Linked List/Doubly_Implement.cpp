a27ae4e16bb4d73db67682d51d3a52504a4cce3b
mohitkr0401 <mohitkr0401@gmail.com>
2025-03-10 17:27:52 +0530

    Add insert functions for singly linked list and implement doubly linked list

---
+++ DSA/Linked List/Doubly_Implement.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Implementing Doubly Linked List
+class Node {
+    public:
+    int data;
+    Node* next;
+    Node* prev;
+    Node(int value) {
+        data = value;
+        next = NULL;
+        prev = NULL;
+    }
+    Node(int data1, Node* next1, Node* prev1) {
+        data = data1;
+        next = next1;
+        prev = prev1;
+    }
+};
+
+// Convert Array to Doubly Linked List
+Node* convertArr2DLL(vector<int> &arr) {
+    Node* head = new Node(arr[0]);
+    Node* prev = head;
+    for (int i = 1; i < arr.size(); i++) {
+        Node* temp = new Node(arr[i], nullptr, prev);
+        prev->next = temp;
+        prev = temp;
+    }
+    return head;
+}
--- DSA/Linked List/Insert.cpp
+++ DSA/Linked List/Insert.cpp
@@ -21,3 +21,62 @@ class Node {
 };
 
 // Insert at the head(Start)
+Node* insertHead(Node* head, int val){
+    // Node* newNode = new Node(val, head);
+    // return newNode;
+    return new Node(val, head); // same as above
+}
+
+// Insert at the tail(End)
+Node* insertTail(Node* head, int val){
+    if (head == nullptr) {
+        return new Node(val);
+    }
+    Node* temp = head;
+    while (temp->next != nullptr) {
+        temp = temp->next;
+    }
+    temp->next = new Node(val);
+    return head;
+}
+
+// Insert at kth position
+Node* insertKth(Node* head, int val, int k) {
+    if (head == nullptr) {
+        if(k==1) return new Node(val);
+        else return NULL;
+    }
+    if (k == 1) {
+        return new Node(val, head);
+    }
+    Node* temp = head;
+    int count = 0;
+    while(temp!=NULL) {
+        count++;
+        if(count == k-1) {
+            temp->next = new Node(val, temp->next);
+            break;
+        }
+        temp = temp->next;
+    }
+    return head;
+}
+
+// Insert element before the given value
+Node* insertBefore(Node* head, int val, int x) {
+    if (head == nullptr) {
+        return NULL;
+    }
+    if (head->data == x) {
+        return new Node(val, head);
+    }
+    Node* temp = head;
+    while(temp->next!=NULL) {
+        if(temp->next->data == x) {
+            temp->next = new Node(val, temp->next);
+            break;
+        }
+        temp = temp->next;
+    }
+    return head;
+}
