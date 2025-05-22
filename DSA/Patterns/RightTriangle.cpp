30e4addcd79c76a45667f7c8e65d95e83f7c28b2
mohitkr0401 <mohitkr0401@gmail.com>
2025-05-22 21:35:53 +0530

    Add pattern printing programs for right number, right triangle, and triangle shapes
    - Implemented RightNumber.cpp to print numbers in a right-angled triangle pattern.
    - Implemented RightTriangle.cpp to print asterisks in a right-angled triangle pattern.
    - Implemented Triangle.cpp to print numbers in a triangular pattern.
    - Each program includes a main function to handle multiple test cases.

---
+++ DSA/Patterns/RightTriangle.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void Pattern(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int n;
+        cin >> n;
+        Pattern(n);
+    }
+    return 0;
+}
---
+++ DSA/Patterns/Triangle.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void Pattern(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int n;
+        cin >> n;
+        Pattern(n);
+    }
+    return 0;
+    // Number of line is printed like a right triangle
+}
