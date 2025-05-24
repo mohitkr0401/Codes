1fdaca119aa7d95bfd01bbaa77d4c32768019786
mohitkr0401 <mohitkr0401@gmail.com>
2025-05-24 22:03:06 +0530

    Add pattern printing programs for reverse number triangle, reverse star triangle, star diamond, and star triangle

---
+++ DSA/Patterns/ReverseNumberTriangle.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void Pattern(int n) {
+	for(int i=n;i>=1;i--){
+		for(int j=1;j<=i;j++){
+			cout<<j<<" ";
+		}
+		cout<<endl;
+	}
+}
+
+int main() {
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int n;
+        cin >> n;
+        Pattern(n);
+    }
+    return 0;
+}
---
+++ DSA/Patterns/StarDiamond.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+void Pattern(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 0; j < 2 * i + 1; j++)
+        {
+            cout << "*";
+        }
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << " ";
+        }
+        cout << endl;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 0; j < 2 * n - (2 * i + 1); j++) // logic: 2*n - (2*i + 1) gives the number of stars to print
+        {
+            cout << "*";
+        }
+        for (int j = 0; j < i; j++)
+        {
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+int main()
+{
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int n;
+        cin >> n;
+        Pattern(n);
+    }
+    return 0;
+}
